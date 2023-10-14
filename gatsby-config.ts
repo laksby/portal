3abecea64877791607c03dd11d0e94993a43e976
laksby <laksby@icloud.com>
2023-10-14 02:42:09 +0200

    Initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,12 @@
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `laksby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  graphqlTypegen: true,
+  plugins: [],
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const IndexPage: FC = () => {
+  return <div>TODO</div>;
+};
+
+export default IndexPage;
