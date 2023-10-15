fad3c50679d1ef83cadc88c96d3a2cd5f6cb9853
laksby <laksby@icloud.com>
2023-10-16 01:17:58 +0200

    Implemented logic of reading data

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,13 +1,21 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  pathPrefix: `/portal`,
+  pathPrefix: '/portal',
   siteMetadata: {
-    title: `laksby`,
-    siteUrl: `https://laksby.github.io/portal/`,
+    title: 'laksby',
+    siteUrl: 'https://laksby.github.io/portal/',
   },
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/data`,
+      },
+    },
+  ],
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,39 @@
+import { PageProps, graphql } from 'gatsby';
 import { FC } from 'react';
 
-export const IndexPage: FC = () => {
-  return <div>TODO</div>;
+export const IndexPage: FC<PageProps<Queries.IndexPageQuery>> = props => {
+  const { data } = props;
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 };
 
 export default IndexPage;
+
+export const query = graphql`
+  query IndexPage {
+    allWorkTitleJson {
+      edges {
+        node {
+          year
+          month
+          company
+          position
+          title
+        }
+      }
+    }
+    allWorkProjectJson {
+      edges {
+        node {
+          index
+          project
+          role
+          product
+          industry
+          activities
+          techs
+        }
+      }
+    }
+  }
+`;
